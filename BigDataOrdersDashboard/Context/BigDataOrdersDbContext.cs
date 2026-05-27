e5eade264d0d734d9cdbca876de22427a517704f
Ahmet Batuhan Avcıoğlu <54512066+Batuhan18@users.noreply.github.com>
2026-05-27 12:36:05 +0300

    ...

---
+++ BigDataOrdersDashboard/Context/BigDataOrdersDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BigDataOrdersDashboard.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BigDataOrdersDashboard.Context
+{
+    public class BigDataOrdersDbContext: DbContext
+    {
+        public BigDataOrdersDbContext(DbContextOptions<BigDataOrdersDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ BigDataOrdersDashboard/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BigDataOrdersDashboard.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BigDataOrdersDashboard.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly BigDataOrdersDbContext _context;
+
+        public CategoryController(BigDataOrdersDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult CategoryList()
+        {
+            var values = _context.Categories.ToList();
+            return View(values);
+        }
+    }
+}
